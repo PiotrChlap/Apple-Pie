04830f9e061e38c21f9ae149e670af5436821392
Piotr Chłapiński <75984836+PiotrChlap@users.noreply.github.com>
2022-06-24 00:08:00 +0200

    Add logic of starting game

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -12,11 +12,28 @@ class ViewController: UIViewController {
     @IBOutlet var correctWordLabel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
     @IBOutlet var letterButtons: [UIButton]!
+    var listOfWords = ["swift", "aligator", "bathroom", "bike", "monkey", "football", "pizza", "chicken"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
+    var currentGame: Game!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newRound()
     }
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, inccorectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI() {
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.inccorectMovesRemaining)")
+    }
+    
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
     }
