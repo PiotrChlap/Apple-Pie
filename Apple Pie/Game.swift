7795ecbc9459e4af90750b5abf36fb1ffe51ac34
Piotr Chłapiński <75984836+PiotrChlap@users.noreply.github.com>
2022-06-24 09:09:27 +0200

    Add revealed word

--- Apple Pie/Game.swift
+++ Apple Pie/Game.swift
@@ -10,6 +10,17 @@ struct Game {
     var word: String
     var inccorectMovesRemaining: Int
     var guessedLetters: [Character]
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter){
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
     
     mutating func playerGuessedLetter(letter: Character) {
         guessedLetters.append(letter)
